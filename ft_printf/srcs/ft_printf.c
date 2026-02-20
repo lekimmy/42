d4bba9e74a4857cf0d92a7e88a9c13e5ec9ec3c2
Le-Kim-My S Phabmixay <ls-phabm@f0r4s12.clusters.42paris.fr>
2026-02-20 03:00:51 +0100

    clean ft_printf

---
+++ ft_printf/srcs/ft_printf.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ls-phabm <ls-phabm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/24 14:57:16 by ls-phabm          #+#    #+#             */
+/*   Updated: 2026/02/20 02:58:06 by ls-phabm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+// Input value instead of pointer, otherwise UB
+static int	ft_format(va_list args, const char *format)
+{
+	if (*format == 'c')
+		return (ft_print_char(va_arg(args, int)));
+	else if (*format == 's')
+		return (ft_print_str(va_arg(args, char *)));
+	else if (*format == 'p')
+		return (ft_print_ptr(va_arg(args, void *)));
+	else if (*format == 'd' || *format == 'i')
+		return (ft_print_nbr(va_arg(args, int)));
+	else if (*format == 'u')
+		return (ft_print_unsigned(va_arg(args, unsigned int)));
+	else if (*format == 'x' || *format == 'X')
+		return (ft_print_hex(va_arg(args, unsigned int), *format));
+	else if (*format == '%')
+		return (ft_print_perc());
+	return (-1);
+}
+
+// In C standard, printf should stop at invalid or incomplete conversion -> UB
+// Must return -1 or ignore safely
+// glibc's output is an implementation choice, not a rule
+int	ft_handle_percent(va_list args, const char *format)
+{
+	int	ret;
+
+	format++;
+	if (!format)
+		return (va_end(args), -1);
+	ret = ft_format(args, (const char *)format);
+	if (ret == -1)
+		return (-1);
+	return (ret);
+}
+
+// Reject invalid or incomplete conversion to avoid UB
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		count_chars;
+	int		ret;
+
+	if (!format)
+		return (-1);
+	count_chars = 0;
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			ret = ft_handle_percent(args, (const char *)format);
+			if (ret == -1)
+				return (va_end(args), -1);
+			count_chars += ret;
+			format++;
+		}
+		else
+			count_chars += ft_print_char(*format);
+		format++;
+	}
+	return (va_end(args), count_chars);
+}
