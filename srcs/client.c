fd302ecea16cd4965f10ecb8c7fc7c6ffdde05e4
Le-Kim-My S Phabmixay <ls-phabm@f0r4s12.clusters.42paris.fr>
2026-02-21 18:43:18 +0100

    server

--- srcs/client.c
+++ srcs/client.c
@@ -6,28 +6,37 @@
 /*   By: ls-phabm <ls-phabm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/21 16:39:28 by ls-phabm          #+#    #+#             */
-/*   Updated: 2026/02/21 16:55:48 by ls-phabm         ###   ########.fr       */
+/*   Updated: 2026/02/21 18:42:59 by ls-phabm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-// int	parsing(void)
-// {
-// }
+static int	is_valid_pid(int pid)
+{
+	if (pid > 0)
+		return (1);
+	return (0);
+}
 
-// int	encrpyt_msg(void)
-// {
-// }
+int	parsing(void)
+{
+}
 
-// int	send_msg(void)
-// {
-// }
+int	encrpyt_msg(void)
+{
+}
+
+int	send_msg(void)
+{
+}
 
 int	main(int argc, char **argv)
 {
-	(void)argv;
 	if (argc != 3)
 		return (1);
+	if (!is_valid_pid(argv[1]))
+		exit(EXIT_FAILURE);
 	ft_printf(1, "mdr");
+	return (0);
 }
