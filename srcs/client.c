1e1ddc51fa548ea0b3ee560c638b8b13bc0c2d06
Le-Kim-My S Phabmixay <ls-phabm@f0r4s12.clusters.42paris.fr>
2026-02-21 18:56:47 +0100

    parsing

--- srcs/client.c
+++ srcs/client.c
@@ -6,21 +6,22 @@
 /*   By: ls-phabm <ls-phabm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/21 16:39:28 by ls-phabm          #+#    #+#             */
-/*   Updated: 2026/02/21 18:42:59 by ls-phabm         ###   ########.fr       */
+/*   Updated: 2026/02/21 18:55:49 by ls-phabm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static int	is_valid_pid(int pid)
+int	parsing(char **argv)
 {
-	if (pid > 0)
-		return (1);
-	return (0);
-}
+	int	pid;
 
-int	parsing(void)
-{
+	pid = ft_atoi(argv[1]);
+	if (!is_digit(pid))
+		return (0);
+	if (pid <= 0)
+		return (0);
+	return (1);
 }
 
 int	encrpyt_msg(void)
@@ -35,7 +36,7 @@ int	main(int argc, char **argv)
 {
 	if (argc != 3)
 		return (1);
-	if (!is_valid_pid(argv[1]))
+	if (!parsing)
 		exit(EXIT_FAILURE);
 	ft_printf(1, "mdr");
 	return (0);
