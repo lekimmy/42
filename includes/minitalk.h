200e7da464d29bbac793d347f2a9ada00558c2f4
Le-Kim-My S Phabmixay <ls-phabm@f0r4s12.clusters.42paris.fr>
2026-02-26 02:05:20 +0100

    wsh

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: ls-phabm <ls-phabm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 17:56:16 by ls-phabm          #+#    #+#             */
-/*   Updated: 2026/02/25 22:44:54 by ls-phabm         ###   ########.fr       */
+/*   Updated: 2026/02/26 02:00:45 by ls-phabm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 # include "ft_printf.h"
 # include "libft.h"
 # include <signal.h>
+
+void	kill_signal(pid_t pid, int signal);
 
 #endif
