f974b6c8c38c69a41c5586d8f86ad2aeb8b29769
Le-Kim-My S Phabmixay <ls-phabm@f0r4s12.clusters.42paris.fr>
2026-05-05 16:57:48 +0200

    lexer

--- minishell/srcs/lexer.c
+++ minishell/srcs/lexer.c
@@ -6,11 +6,39 @@
 /*   By: ls-phabm <ls-phabm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 15:26:18 by ls-phabm          #+#    #+#             */
-/*   Updated: 2026/05/05 15:27:12 by ls-phabm         ###   ########.fr       */
+/*   Updated: 2026/05/05 16:57:38 by ls-phabm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+t_token	*new_token(t_token_type type, char *value)
+{
+	t_token	*token;
+
+	token = malloc(sizeof(t_token));
+	if (!token)
+		return (NULL);
+	token->type = type;
+	token->value = value;
+	token->next = NULL;
+	return (token);
+}
+
+void	add_token(t_token **head, t_token *new_token)
+{
+	t_token	*current;
+
+	if (!*head)
+	{
+		*head = new_token;
+		return ;
+	}
+	current = *head;
+	while (current->next)
+		current = current->next;
+	current->next = new_token;
+}
 
 void tokenize(char *line)
 {
