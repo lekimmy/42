902862c4fc69dece7727fa113ed3edf052f7f360
Le-Kim-My S Phabmixay <ls-phabm@f0r4s12.clusters.42paris.fr>
2026-05-06 18:47:59 +0200

    quotes samere

--- minishell/srcs/lexer_quotes.c
+++ minishell/srcs/lexer_quotes.c
@@ -6,20 +6,34 @@
 /*   By: ls-phabm <ls-phabm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 17:01:17 by ls-phabm          #+#    #+#             */
-/*   Updated: 2026/05/06 18:31:01 by ls-phabm         ###   ########.fr       */
+/*   Updated: 2026/05/06 18:44:24 by ls-phabm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+// quotes don't split words > they override splitting
+// outside quotes, spaces split words
+// inside, spaces are just characters
+
+// 1. detect quotes
+// 2. read until matching quote
+// 3. include content in word
+// 4. (ignore expansion for now)
 
 // int	quote_opened()
 // {
 	
 // }
 
-// int	quote_not_closed()
+// int	quote_closed()
 // {
 	
+// }
+
+// int	in_quotes()
+// {
+//		return (quote_opened() + quote_closed == 2);
 // }
 
 void syntax_error(char c)
