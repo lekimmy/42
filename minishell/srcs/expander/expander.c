66c792490a90739026fc18fbcbcd03ea8f37af1f
Le-Kim-My S Phabmixay <ls-phabm@f0r4s12.clusters.42paris.fr>
2026-05-25 22:34:38 +0200

    expand draft

--- minishell/srcs/expander/expander.c
+++ minishell/srcs/expander/expander.c
@@ -6,7 +6,7 @@
 /*   By: ls-phabm <ls-phabm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/23 20:16:29 by ls-phabm          #+#    #+#             */
-/*   Updated: 2026/05/23 22:44:18 by ls-phabm         ###   ########.fr       */
+/*   Updated: 2026/05/25 22:33:56 by ls-phabm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,44 @@
 // {
 // 	return ("lol");
 // }
+
+char	*extract_var(char *str, size_t *i)
+{
+	
+}
+
+size_t	expanded_len(char *s, t_env *env, int exit_code)
+{
+	while (*s)
+	{
+		
+	}
+}
+
+char	*expand_string(char *s, t_env *env, int exit_code);
+{
+	size_t	i;
+	size_t	len;
+	char	*buf;
+	
+	len = expanded_len(s, env, exit_code);
+	buf = malloc(len + 1);
+	i = 0;
+	while ()
+	{
+		
+	}
+	buf[i] = '\0';
+	return (buf);
+}
+
+char	*expand_segment(t_segment *seg, t_env *env, int exit_code)
+{
+	if (seg->quote_context == 1)
+		return ;
+	while (seg->value)
+	{
+		if (*(seg->value) == '$')
+			expand_string(seg->value, env, exit_code);
+	}
+}
